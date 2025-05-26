02fbe003374da6cfd123561bbeed28c46765f74d
Minyong Kim <ravenade88@gmail.com>
2025-05-26 22:14:16 +0900

    UI: 로그인 페이지 퍼블리싱 완료

---
+++ src/components/LoginPage/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="text-white h-[92px]">
+            <h1 className="text-xl font-bold pl-[166.75px] pt-[24px]">로고</h1>
+        </header>
+    );
+};
+
+export default Header;
