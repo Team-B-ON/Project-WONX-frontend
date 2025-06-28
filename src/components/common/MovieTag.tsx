9aa6592abbaf2c8e40683564e45c8e5befb24137
Minyong Kim <ravenade88@gmail.com>
2025-06-28 20:35:42 +0900

    UI: MovieTag 간단한 UI 수정

--- src/components/common/MovieTag.tsx
+++ src/components/common/MovieTag.tsx
@@ -8,15 +8,15 @@ type MovieTagProps = {
 
 const MovieTag = ({ title, onClickMore, showMore = true }: MovieTagProps) => {
   return (
-    <div className="flex items-center px-4 group space-x-2">
-      <h2 className="text-white text-xl font-bold">
+    <div className="flex items-center group space-x-2 cursor-pointer">
+      <h2 className="text-[rgb(229,229,229)] group-hover:text-white text-xl font-bold">
         {title}
       </h2>
 
       {showMore && (
         <button
           onClick={onClickMore}
-          className="text-sm text-blue-300 opacity-0 group-hover:opacity-100 transition-opacity duration-300"
+          className="text-sm text-blue-300 opacity-0 group-hover:opacity-100 transition-opacity duration-300 cursor-pointer"
         >
           모두 보기 &gt;
         </button>
