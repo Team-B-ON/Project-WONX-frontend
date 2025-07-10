0dbfa927d220359af36a11e7a772a26bd7febe19
O-Dong <it0825@naver.com>
2025-07-10 15:56:27 +0900

    update: 검색 api 연동 밑작업

---
+++ src/types/searchResult.ts
@@ -0,0 +1,27 @@
+export interface MovieSearchDto {
+  id: string;
+  title: string;
+  posterUrl: string;
+}
+export interface UserSearchDto {
+  id: string;
+  nickname: string;
+  avatarUrl: string;
+}
+export interface ReviewSearchDto {
+  id: string;
+  movieTitle: string;
+  content: string;
+  author: string;
+  rating: number;
+}
+export interface SuggestionDto {
+  keyword: string;
+}
+
+export interface SearchResult {
+  movies: MovieSearchDto[];
+  users: UserSearchDto[];
+  reviews: ReviewSearchDto[];
+  suggestions?: SuggestionDto[];
+}
